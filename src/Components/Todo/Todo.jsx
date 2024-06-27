3c34873229ad96e9581639c566ab68b7f465c558
Ayushkr7380 <ayushkumar19999@gmail.com>
2024-06-27 17:15:53 +0530

    todo heading added

--- src/Components/Todo/Todo.jsx
+++ src/Components/Todo/Todo.jsx
@@ -4,6 +4,11 @@ function Todo(){
     return(
         <>
             <div id="TodoBody">
+                <h1 style={{
+                    color:'white',
+                    textAlign:"center",
+                    marginTop:'1rem'
+                }}>Simple Todo</h1>
                 <AddTodo/>
             </div>
         </>
